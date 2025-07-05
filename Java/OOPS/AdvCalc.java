6c68604116d4186b7f418b5bff928ec9f2bb2494
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-05 11:48:44 +0530

    Example of Inheritance

---
+++ Java/OOPS/AdvCalc.java
@@ -0,0 +1,11 @@
+public class AdvCalc extends Calc {
+    public int multiply(int n1 , int n2)
+    {
+        return n1*n2;
+    }
+
+    public int division(int n1 , int n2)
+    {
+        return n1/n2;
+    }
+}
---
+++ Java/OOPS/Inheritance.java
@@ -0,0 +1,8 @@
+
+public class Inheritance {
+    public static void main(String[] args) {
+        AdvCalc obj = new AdvCalc();
+        System.out.println(obj.add(7,3));
+        System.out.println(obj.multiply(7,3));
+    }
+}
