5f2578572a0e533feaac9e50abdd6a7c2dd19f4c
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-05 12:26:17 +0530

    Information about Multiple Inheritance and why its not supported in java

--- Java/OOPS/Inheritance.java
+++ Java/OOPS/Inheritance.java
@@ -1,4 +1,17 @@
-
+//Two Parents one child called Multiple Inheritance it is possible in C++ Is it possible in java too?
+//No we cant but why ?
+//Because imagine if both parents have method y and when we call obj.y() it will createa confusion that from whom should I inherit
+//This problem is called Ambiguity problem
+//class A{
+//
+//}
+//class B{
+//
+//}
+//class C extends A , B
+//{
+//
+//}
 public class Inheritance {
     public static void main(String[] args) {
         VeryAdvCalc obj = new VeryAdvCalc();
@@ -7,3 +20,5 @@ public class Inheritance {
         System.out.println(obj.power(10,2));
     }
 }
+
+//But there might be a way of doing it by using interfaces
