e77872c3e83c9a2e11633bc8bf56eadafeafda5f
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-08 22:37:51 +0530

    Combinaton of abstract and Anonymous Innerclass

--- Java/AnonymousInnerClass.java
+++ Java/AnonymousInnerClass.java
@@ -1,8 +1,13 @@
-class One {
-    public  void show()
-    {
-        System.out.println("in Show ");
-    }
+//class One {
+//    public  void show()
+//    {
+//        System.out.println("in Show ");
+//    }
+//}
+//Combination of Abstract and InnercLASS
+abstract class One{
+    public abstract void show(); //I fYou want to implement the abstract or interface only once we can do this combination
+    public abstract void config();
 }
 public class AnonymousInnerClass {
     public static void main(String[] args) {
@@ -12,7 +17,13 @@ public class AnonymousInnerClass {
             {
                 System.out.println("In new Show ");
             }
+            public void config()
+            {
+                System.out.println("In config show ");
+            }
         };
-        obj.show(); // priority will be given to the show method of main method
+        obj.show();
+        // priority will be given to the show method of main method
+        obj.config();
     }
 }
