963e69e18e89144716ef833ee1ce3d200664e03f
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-10 05:24:15 +0530

    More on Enuma

--- Java/Enummeration.java
+++ Java/Enummeration.java
@@ -5,11 +5,32 @@ enum Status{
     FAILED;
 //Status is a class and inside it are called objects
 }
-public class Enum {
+enum Laptop{
+    MacBook(2000),XPS(2200),Surface,ThinkPad(1800);
+    private int price;
+    private Laptop()
+    {
+        this.price = 500;
+    }
+    private Laptop(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {// here we are using private constuctor because we are creating the object (i.e Macbook etc)
+//in the same class
+       this.price = price;
+    }
+}
+public class Enummeration {
     public static void main(String[] args) {
        // int i = 5 ;
         Status s = Status.RUNNING;
         System.out.println(s.ordinal());
+        System.out.println(s.getClass().getSuperclass());
 
         switch(s)
         {
@@ -38,5 +59,18 @@ public class Enum {
         /*Status[] a = Status.values();
         for(Status n : a)
             System.out.println(n + ":" + n.ordinal());*/
+        Laptop lp = Laptop.MacBook;
+        System.out.println(lp.getPrice());
+
+        for(Laptop lap : Laptop.values())
+        {
+            System.out.println(lap + " : " + lap.getPrice());
+        }
+
     }
 }
+
+//Enum is a class but we cant extend it
+//You can create constructor and also define variables inside enum
+
+//Bydefault enum in java extends a Enum class
