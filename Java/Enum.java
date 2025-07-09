f90e40dd91260a37a33cfbadfa98c5bb9ea86a98
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-09 22:27:15 +0530

    Enum Concept in IF and Switch

--- Java/Enum.java
+++ Java/Enum.java
@@ -7,11 +7,36 @@ enum Status{
 }
 public class Enum {
     public static void main(String[] args) {
-        int i = 5 ;
+       // int i = 5 ;
         Status s = Status.RUNNING;
         System.out.println(s.ordinal());
-        Status[] a = Status.values();
+
+        switch(s)
+        {
+            case RUNNING :
+                System.out.println("ALL GOOD");
+                break;
+            case FAILED:
+                System.out.println("Try Again");
+                break;
+            default:
+                System.out.println("Done");
+                break;
+
+        }
+        if(s == Status.RUNNING)
+        {
+            System.out.println("ALL GOOD");
+        } else if (s == Status.COMPLETED) {
+            System.out.println("Done");
+        } else if (s == Status.FAILED) {
+            System.out.println("Try Again");
+        }
+        else {
+            System.out.println("Finished");
+        }
+        /*Status[] a = Status.values();
         for(Status n : a)
-            System.out.println(n + ":" + n.ordinal());
+            System.out.println(n + ":" + n.ordinal());*/
     }
 }
