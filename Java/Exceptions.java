932e1cf394b30ad7d4d735c7b3389304ac3f9e6d
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-10 07:54:08 +0530

    Exceptions with multiple catch

--- Java/Exceptions.java
+++ Java/Exceptions.java
@@ -9,16 +9,32 @@
 public class Exceptions {
     public static void main(String[] args) {
         //statements can be normal statement or critical statement
-        int i =4;
+        int i =2;
         int j = 0;
+        int nums[] = new int[5];
+        String str = null;
         try
         {
-            j=18/i;
+            System.out.println(str.length());
+            j=18/i;// Whenever we get the exception over here it directly jump to
+            // the exception ignoring other statements in try block
+            System.out.println(nums[5]);
         }
-        catch (Exception e) //Java here throws an exception ou need to catch it and it throws an object
-        {// this block will be executed only at the time of exception itherwise it will get skipped
-            System.out.println("Something went wrong");
+//        catch (Exception e) //Java here throws an exception ou need to catch it and it throws an object
+//        {// this block will be executed only at the time of exception itherwise it will get skipped
+//            System.out.println("Something went wrong " + e);
+//        }
+        catch(ArithmeticException e )
+        {
+            System.out.println("Cannot divide by zero ");
         }
+        catch (ArrayIndexOutOfBoundsException e )
+        {
+            System.out.println("You are accessing the element out of the array ");
+        } catch (Exception e) {//Parent class of Exception we can use this because we dont know other that
+            // this two some exception or error might happen so we need to hanldle it
+            System.out.println("Something went wrong " + e);
+        }// this block keep it at the end
         System.out.println(j);
         System.out.println("Bye");
     }
