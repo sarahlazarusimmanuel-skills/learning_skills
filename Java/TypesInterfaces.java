c10da6f538843372caf741b10f0fb9da401fc6f7
sarahlazarusimmanuel-skills <sarahlazarusimmanuel10082000@gmail.com>
2025-07-17 15:40:02 +0530

    Var in Java and Local Variable Type Inference concept

--- Java/TypesInterfaces.java
+++ Java/TypesInterfaces.java
@@ -11,7 +11,7 @@
 //Deserialization to load the values into the object instead of default values //by default every object is not allowed to that
 //you have to give permission through MarkerInterface
 @FunctionalInterface
-interface C{
+interface D{
     void show();
 }
 //class D implements C{
@@ -21,13 +21,28 @@ interface C{
 //    }
 //}
 public class TypesInterfaces {
+    //var num = 10; //Instance variables // 'var' is not allowed here
+
     public static void main(String[] args) {
-        C obj = new C() {//Anonymous
+        D obj = new D() {//Anonymous
             public void show() {
                 System.out.println("In Show");
             }
         };
         obj.show();
+        //LVTI ->Local Variable TypeInference
+        int a = 9;
+        var b = 8;// is also in integer datatype //type will be assigned by compiler
+        int c;
+        var d = 10;                  // cannot infer type for local variable d
+
+        // cannot use 'var' on variable without initializer
+        //
+        String var ="Sarah";
+        //dont make your class var it will not work
+        //int nums[] = new int[10];
+        var nums = new int[10]; //no need of square brackets
+        var obj = new Alien();
     }
 }
 //We can use Lambda expressions only with functional Interface
